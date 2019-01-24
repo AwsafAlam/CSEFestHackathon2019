bc32443691390f6d85477c0523ee73d717329948
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2019-01-24 16:02:08 +0600

    Main UI modification

---
+++ MyHealth/app/src/main/java/io/github/utshaw/myhealth/StepCount.java
@@ -0,0 +1,13 @@
+package io.github.utshaw.myhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StepCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_count);
+    }
+}
